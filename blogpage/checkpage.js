f0ae016f1d7c9581f2c709514ec3ce1eb4f73139
Khiem G Luong <78592170+smarttarded@users.noreply.github.com>
2021-06-09 15:04:02 -0700

    blog post

--- blogpage/checkpage.js
+++ blogpage/checkpage.js
@@ -5,10 +5,14 @@ function checkE() {
 
   var offendedchoice = document.querySelector('input[name="offendedquiz"]:checked').value
   console.log(offendedchoice);
+  var thinkchoice = document.querySelector('input[name="offendedquiz"]:checked').value
+  console.log(thinkchoice)
+
   var ironychoice = document.querySelector('input[name="ironyquiz"]:checked').value
+  var stupidchoice = document.querySelector('input[name="stupidquiz"]:not(:checked)').value
+  console.log(stupidchoice)
 
-
-  if(offendedchoice == "definitely" && ironychoice == "definitely")
+  if(offendedchoice == "definitely" && ironychoice == "definitely" && stupidchoice == "definitelynot")
   {
     close.onclick = function () {
       mainPage.style.display = "flex";
