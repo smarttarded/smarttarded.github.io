9146fc64d818025ae05586b1716f9ca192ebf19f
Khiem G Luong <78592170+smarttarded@users.noreply.github.com>
2021-06-18 19:55:59 -0700

    reformat

--- blogpage/checkpage.js
+++ blogpage/checkpage.js
@@ -4,22 +4,24 @@ function checkE() {
   var close = document.getElementById('btnClose');
 
   var offendedchoice = document.querySelector('input[name="offendedquiz"]:checked').value
-  console.log(offendedchoice);
+
   var thinkchoice = document.querySelector('input[name="offendedquiz"]:checked').value
-  console.log(thinkchoice)
-
   var ironychoice = document.querySelector('input[name="ironyquiz"]:checked').value
-  var stupidchoice = document.querySelector('input[name="stupidquiz"]:not(:checked)').value
-  console.log(stupidchoice)
+  var stupidchoice = document.querySelector('input[name="stupidquiz"]:checked')
+  var employeechoice = document.querySelector('input[name="employeequiz"]:checked')
 
-  if(offendedchoice == "definitely" && ironychoice == "definitely" && stupidchoice == "definitelynot")
+  if(offendedchoice == "definitely" && ironychoice == "definitely" && thinkchoice == "definitely" && employeechoice == null && stupidchoice == null)
   {
     close.onclick = function () {
       mainPage.style.display = "flex";
       form.style.display = "none";
-
     };
   }
+  else
+  {
+      alert("Unfortunately, we will not be moving forward with your application, but we appreciate your time and interest. - PR")
+      document.getElementById("formID").reset();
+    }
 }
 
 function showSlide(){
